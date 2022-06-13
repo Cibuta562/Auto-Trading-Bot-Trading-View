b7f3e715c4065fef7e61fac97075d24b86403bad
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-06-13 20:39:33 +0300

    Just some imports

--- pythonProject13/main.py
+++ pythonProject13/main.py
@@ -1,21 +1,31 @@
+import json, config
 from flask import Flask, request, jsonify
-import json
 
 app = Flask(__name__)
 
+
+###########imports############
 
 @app.route('/')
 def nu_sex():
     return "Merge !! "
 
-@app.route('/auto_trade' , methods= ['POST'])
+
+@app.route('/auto_trade', methods=['POST'])
 def auto_trade():
     data = json.loads(request.data)
-    print(request)
+
+    if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+        return {
+            "code": "error"
+        }
+
+    print(data['ticker'])
 
     return {"code": "success",
             "message": data
             }
+
 
 if __name__ == '__main__':
     app.run()
